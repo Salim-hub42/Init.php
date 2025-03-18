76323a2c211a7130efb00ad8069cf03493364281
salim-hub42 <salim.khalfoun@yahoo.fr>
2025-03-18 12:02:45 +0100

    sa révise !

--- index.php
+++ index.php
@@ -13,15 +13,21 @@
     </header>
     <main>
         <?php
-        $heure_connecxion = (17);
-        if ($heure_connecxion < 18) {
-            echo "Passez une bonne journée !<br>";
-            $journee = "oui";
-        } else {
-            echo "Passez une bonne soirée !<br>";
-            $journee = "non";
-        }
-        echo "fait-il jour ? La réponse est $journee";
+     $note = 13;
+     $dificulte = "facile";
+     $dificulte = "dificile";
+
+     if($note <= 12 && $dificulte == "facile"){
+        echo"c'est null !";
+     }elseif($note >=12  && $dificulte == "facile"){
+        echo "Pas mal , mais c'etait facile";
+     }elseif($note <=12 && $dificulte == "dificile" ){
+        echo "C'est pas terrible et c'etait difficile";
+     }else{
+        echo " Bravo , c'etait difficile mais tu as reussi";
+     }
+
+       
         ?>
     </main>
     <footer>
