660b2d3cc42b7e6194682af9d00a746bb7191cbd
salim-hub42 <salim.khalfoun@yahoo.fr>
2025-03-21 15:10:14 +0100

    Quelque fonctionnalités string (php)

--- index.php
+++ index.php
@@ -4,34 +4,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les boucles en PHP</title>
+    <title>Fonctions String  PHP</title>
 </head>
 
 <body>
-    <header>
+<header>
 
-    </header>
-    <main>
-        <?php
-        $menbres = array(
-            array('Sal', 35, 'vico42000@yahoo.fr'),
-            array('Ass', 36, 'ass@yahoo.com'),
-            array('Zak', 6, 'Zak@yahoo.com'),
-        );
-        for ($ligne = 0; $ligne < 3; $ligne++) {
-            $menbres_no = $ligne + 1;
-            echo 'Menbre numéro'  . $menbres_no .  '<br>';
-            echo '<ul>';
-            for ($col = 0; $col < 3; $col++) {
-                echo '<li>' . $menbres[$ligne][$col] . '</li>';
-            }
-            echo '</ul>';
-        }
-        ?>
-    </main>
-    <footer>
+</header>
+<main>
+<?php
+echo strlen("Bonjour a tous")."<br>";
+echo str_word_count("Bonjour à tous")."<br>";
+echo str_repeat("Coucou à tous <br>", 10);
+echo str_replace("Bonjour", "Bonsoir", "Bonjour à tous")."<br>";
+echo strtolower("BONJOUR A TOUS")."<br>";
+echo strtoupper("bonjour a tous")."<br>";
+echo strpos("Bonjour à tous", "tous")."<br>";
 
-    </footer>
+$str = 'j\'aime les fruits et le <strong>chocolat</strong>';
+echo htmlspecialchars($str)."<br>";
+echo htmlspecialchars_decode($str)."<br>";
+
+echo nl2br("Bonjour à tous \n Comment ça va ?")."<br>";
+
+
+
+
+?>
+</main>
+<footer>
+
+</footer>
 </body>
 
 </html>
