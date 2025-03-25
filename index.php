37f2a67a57b0bf1047dde154a523b5f523bbaaa6
salim-hub42 <salim.khalfoun@yahoo.fr>
2025-03-25 15:27:36 +0100

    quelque fonction temporel

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fonctions String  PHP</title>
+    <title>Fonctions relative au temp</title>
 </head>
 
 <body>
@@ -13,25 +13,83 @@
 </header>
 <main>
 <?php
-echo strlen("Bonjour a tous")."<br>";
-echo str_word_count("Bonjour à tous")."<br>";
-echo str_repeat("Coucou à tous <br>", 10);
-echo str_replace("Bonjour", "Bonsoir", "Bonjour à tous")."<br>";
-echo strtolower("BONJOUR A TOUS")."<br>";
-echo strtoupper("bonjour a tous")."<br>";
-echo strpos("Bonjour à tous", "tous")."<br>";
+date_default_timezone_set("Europe/Paris");
+   /*  premier exemple sur la gestion des dates
+$jour = array(
+    "",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+    "Dimanche");
 
-$str = 'j\'aime les fruits et le <strong>chocolat</strong>';
-echo htmlspecialchars($str)."<br>";
-echo htmlspecialchars_decode($str)."<br>";
+$mois = array(
+    "",
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre");
 
-echo nl2br("Bonjour à tous \n Comment ça va ?")."<br>";
+    $datefr = $jour[date("N")]." ".date("d")." ".$mois[date("n")]." ".date("Y");
+    echo "Nous sommes le $datefr<br>"; 
+*/
+
+   /* deuxième exemple sur la classe IntlDateFormatter qui est une méthode optimisé pour la gestion des dates
+$formatter = new IntlDateFormatter(
+    'fr_FR',
+    IntlDateFormatter::FULL,
+    IntlDateFormatter::NONE,
+    'Europe/Paris',
+    IntlDateFormatter::GREGORIAN
+);
+echo "Nous sommes le " . $formatter->format(new DateTime());
+*/
+
+
+    // troisième exemple vérification de la validité d'une date
+/*
+$dates = [
+    [12, 31, 2014],
+    [13, 31, 2014],
+    [01, 25, -100]
+];
+
+foreach ($dates as $date) {
+    if (checkdate($date[0], $date[1], $date[2])) {
+        echo "La date " . implode("/", $date) . " est valide<br>";
+    } else {
+        echo "La date " . implode("/", $date) . " n'est pas valide<br>";
+    }
+}
+*/
+echo "<pre>";  
+print_r (getdate());
+echo "</pre>";
+
+echo "<br>";
+echo "<br>";
+
+echo "<pre>";
+print_r (getdate(1));
+echo "<\pre>";
+
+
 
 
 
 
 ?>
-</main>
+</main> 
 <footer>
 
 </footer>
