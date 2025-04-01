a8cdf1171efb86aeb88dde277b80d1cee9828c80
salim-hub42 <salim.khalfoun@yahoo.fr>
2025-04-01 15:42:22 +0200

    Requete et préparation bdd

--- forma.php
+++ forma.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les base de la P O O  en php</title>
+    <title>PHP et MySql</title>
 </head>
 
 <body>
@@ -13,30 +13,34 @@
 </header>
 <main>
 <?php
-
-// Exemple d'utilisation de try et catch pour gérer une connexion à une base de données
-
-// Informations de connexion
-$host = 'localhost'; // Adresse du serveur de base de données
-$dbname = 'test_db'; // Nom de la base de données
-$username = 'root'; // Nom d'utilisateur pour la connexion
-$password = ''; // Mot de passe pour la connexion
+$serveur = "localhost";
+$login = "root";
+$mdp = "";
 
 try {
-    // Tentative de connexion à la base de données avec PDO
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password); // Création d'une instance PDO pour se connecter à la base de données 
+    $connexion = new PDO("mysql:host=$serveur;port=3307;dbname=test2", $login, $mdp);
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Configuration des options PDO pour gérer les erreurs
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Configuration du mode d'erreur de PDO pour lancer des exceptions en cas d'erreur 
+    // Préparation de la requête avec des paramètres
+    $insertion = "INSERT INTO Visiteurs(nom, prenom, email) VALUES (:nom, :prenom, :email)";
+    $stmt = $connexion->prepare($insertion);
 
-    // Si la connexion réussit, afficher un message
-    echo "Connexion réussie à la base de données !"; // Message de succès de la connexion à la base de données
-} catch (PDOException $e) { // Capture des exceptions liées à PDO (exemple : erreur de connexion, base de données inexistante)
-    
-    echo "Erreur de connexion : " . $e->getMessage(); 
-} finally {
-    // Bloc exécuté dans tous les cas (connexion réussie ou échouée)
-    echo "\nFin du script.";
+    // Liaison des paramètres avec leurs valeurs
+    $stmt->bindParam(':nom', $nom);
+    $stmt->bindParam(':prenom', $prenom);
+    $stmt->bindParam(':email', $email);
+
+    // Définition des valeurs
+    $nom = 'khalfoun';
+    $prenom = 'salim';
+    $email = 'salim.khalfoun@yahoo.fr';
+
+    // Exécution de la requête
+    $stmt->execute();
+
+    echo "Insertion réussie !";
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
 }
 ?>
 </main> 
